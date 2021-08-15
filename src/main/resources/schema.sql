100683ea3e6f99c16f066cb85402172e37bd2e44
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-08-15 15:36:05 -0400

    created addDrink, editDrink, rootpage

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE easy_drinks (
+                             id INT NOT NULL Primary Key AUTO_INCREMENT,
+                             drink_name VARCHAR(16),
+                             main VARCHAR(20),
+                             amount1 DOUBLE,
+                             second VARCHAR(20),
+                             amount2 DOUBLE,
+                             directions VARCHAR(250));
